322cbb72aaf75bcfa9381cbab412adfb00b0eeb4
naim <naimaffindi.ptsb@gmail.com>
2024-11-19 21:31:19 +0800

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './templates/dashboard/Dashboard';
+
+function App() {
+  return (
+    <Dashboard />
+  )
+}
+
+export default App;
