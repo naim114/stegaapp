d6bdefa0cdc9fa0917efe458aa6ee12fd8dadf82
naim <naimaffindi.ptsb@gmail.com>
2024-11-20 14:19:04 +0800

    Steganalysis

--- frontend/src/module/report/Report.js
+++ frontend/src/module/report/Report.js
@@ -5,7 +5,7 @@ import Box from '@mui/material/Box';
 export default function Report() {
     return (
         <Box sx={{ width: '100%', maxWidth: { sm: '100%', md: '1700px' } }}>
-            <Typography component="h2" variant="h3" sx={{ mb: 2 }}>
+            <Typography component="h4" variant="h4" sx={{ mb: 1 }}>
                 Report
             </Typography>
         </Box>
--- frontend/src/module/steganalysis/Steganalysis.js
+++ frontend/src/module/steganalysis/Steganalysis.js
@@ -1,13 +1,52 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { Button } from '@mui/material';
 
 export default function Steganalysis() {
+    const [file, setFile] = React.useState(null);
+
+    function handleChange(e) {
+        const selectedFile = e.target.files[0];
+        if (selectedFile) {
+            setFile(URL.createObjectURL(selectedFile));
+        }
+    }
+
     return (
         <Box sx={{ width: '100%', maxWidth: { sm: '100%', md: '1700px' } }}>
-            <Typography component="h2" variant="h3" sx={{ mb: 2 }}>
+            <Typography component="h4" variant="h4" sx={{ mb: 1 }}>
                 Steganalysis
             </Typography>
+            <Typography component="p" variant="p" sx={{ mb: 3, fontWeight: 'bold' }}>
+                Upload an image to do steganalysis, then click ‘Go’.
+            </Typography>
+
+            {/* Step 1 */}
+            <Typography component="p" variant="p" sx={{ mb: 1 }}>
+                Step 1: Choose an image for steganalysis
+            </Typography>
+            <input type="file" onChange={handleChange} />
+
+            {/* Step 2 */}
+            {file && (
+                <>
+                    <Typography component="p" variant="p" sx={{ mt: 4, mb: 1 }}>
+                        Step 2: Preview your image
+                    </Typography>
+                    <img src={file} alt="Preview" style={{ height: 300 }} />
+                </>
+            )}
+
+            {/* Step 3 (conditionally render this section if a file is selected) */}
+            {file && (
+                <>
+                    <Typography component="p" variant="p" sx={{ mt: 4, mb: 1 }}>
+                        Step 3: Submit the image for steganalysis
+                    </Typography>
+                    <Button variant="contained">Steganalyse Now</Button>
+                </>
+            )}
         </Box>
     );
 }
